94f03ce84df23e78d4fd41364a9f921c6d82d2aa
Indrani Som <indranisom39@gmail.com>
2023-11-18 07:55:39 -0800

    feat update:fixed the broken link in navbar2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-
+import { NavLink } from "react-router-dom";
 import { Canvas } from '@react-three/fiber';
 import { Html } from '@react-three/drei';
 import { IoHomeOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineLibraryBooks } from "react-icons/md";
 import { IoIosContact } from "react-icons/io";
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Project from '../pages/Project';
+import Contact from '../pages/Contact';
 import '../Css/nav.css'
 
 const navbar = () => {
   return (
-    <Canvas className='bg-transparent p-0 m-0'>
-      <Html>
-        
+    
+  
     <nav>
-  <a href='src\pages\Home.jsx'><IoHomeOutline /><div className='con'>Home</div></a >
-  <a href='src\pages\About.jsx'><FaRegUser /><div className='con'>About Me</div></a >
-    <a href ='src\pages\Project.jsx'><MdOutlineLibraryBooks /><div className='con'>My Projects</div></a >
-    <a href ='src\pages\Contact.jsx'><IoIosContact /><div className='con'>ContactMe</div></a >
+<NavLink to='/' ><IoHomeOutline /><div className='con'>Home</div></NavLink >
+<NavLink to='/About'><FaRegUser /><div className='con'>About Me</div></NavLink >
+ <NavLink to ='/Project'><MdOutlineLibraryBooks /><div className='con'>My Projects</div></NavLink >
+ <NavLink to ='/Contact'><IoIosContact /><div className='con'>ContactMe</div></NavLink >
   </nav>
   
-  </Html>
-  </Canvas>   
+
+  
   )
 }
 
