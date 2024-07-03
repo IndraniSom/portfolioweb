682a82c3a9f61d094e0420a22767dabde7360b33
IndraniSom <indranisom39@gmail.com>
2024-07-03 17:12:33 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,11 +24,11 @@ const Home = () => {
     <img src="/homeme.png" alt="model" className='hidden md:flex w-100 h-100'/>
     </div>
       <div className='flex flex-col items-end justify-end pt-9 md:px-20 px-10 '>
-      <p className="text-[#131842] text-3xl md:text-7xl font-dreamavenue font-semibold">HELLO THERE👋<br/> I AM INDRANI</p>
+      <p className="text-[#131842] text-3xl md:text-7xl font-recursive font-medium">HELLO THERE👋<br/> I AM INDRANI</p>
       {/* <h2 className="text-white text-8xl font-dreamavenue font-semibold"> I am Indrani Som  </h2> */}
       
       {/* <div className='flex font-dreamavenue2 text-2xl '><p className="text-white text-2xl font-dreamavenue2 font-normal pr-64">I am a </p> <FlipWords className="text-white" words={words} /></div> */}
-      <div className='flex flex-row pr-0 md:pr-60 gap-2'>
+      <div className='flex flex-row pr-0 md:pr-[17rem] gap-2'>
       <a href="mailto:indranisomofficial@gmail.com"><button className=' text-black-500 p-1.5 md:p-3 rounded-md mt-4 bg-green '>Get in touch</button></a>
       <a href="src/assets/Resume.pdf" download><button className=' text-black-500 p-1.5 md:p-3 rounded-md mt-4 ml-4 bg-green '>Download Resume</button></a>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,8 @@ module.exports = {
         dreamavenue:["Dream Avenue", "cursive"],
         dreamavenue2:["Dream Avenue", "sans-serif"],
         worksans: ["Work Sans", "sans-serif"],
-        poppins: ['Poppins', "sans-serif"]
+        poppins: ['Poppins', "sans-serif"],
+        recursive: ['Recursive', "sans-serif"],
       },
       boxShadow: {
         card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
