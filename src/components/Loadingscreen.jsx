d5c7e7a8bb9309610aa9d90ad68f8c4e0f9068b4
IndraniSom <indranisom39@gmail.com>
2024-07-05 01:39:56 +0530

    preloader added

--- src/components/Loadingscreen.jsx
+++ src/components/Loadingscreen.jsx
@@ -1,32 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { gsap } from 'gsap';
+import React, { useEffect, useState } from "react";
+import { gsap } from "gsap";
 
 export function LoadingScreen() {
   // State to hold the loading percentage
   const [loadingPercentage, setLoadingPercentage] = useState(0);
 
   useEffect(() => {
-    const progressBar = document.getElementById('progressBar');
+    const progressBar = document.getElementById("progressBar");
 
     gsap.to(progressBar, {
-      width: '100%',
+      width: "100%",
       duration: 1, // Duration of 2 seconds
-      ease: 'linear',
+      ease: "linear",
       onUpdate: () => {
         // Calculate the current progress
-        const progress = Math.round(gsap.getProperty(progressBar, 'width') / gsap.getProperty(progressBar.parentNode, 'width') * 100);
+        const progress = Math.round(
+          (gsap.getProperty(progressBar, "width") /
+            gsap.getProperty(progressBar.parentNode, "width")) *
+            100
+        );
         // Update state with the new loading percentage
         setLoadingPercentage(progress);
-      }
+      },
     });
   }, []);
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-background dark:bg-blackbg bg-grid-black/[0.2]  dark:bg-dot-black-500/[0.3]">
-      <div  className="mt-5 text-4xl md:text-9xl font-extrabold text-black">Are you ready?💡 </div>
+      <div className="mt-5 text-4xl md:text-9xl font-extrabold text-black">
+        Are you ready?💡{" "}
+      </div>
       {/* <img src='/preloader.png' className='w-1/2 h-1/2 object-contain' alt='preloader' /> */}
       {/* Display the dynamic loading percentage */}
-      <div id="loadingText" className="mt-5 text-7xl md:text-9xl font-extrabold text-black">{loadingPercentage}%</div>
+      <div
+        id="loadingText"
+        className="mt-5 text-7xl md:text-9xl font-extrabold text-black"
+      >
+        {loadingPercentage < 4 ? "1%" : loadingPercentage < 6 ? "25%" : "100%"}
+      </div>
       <div className="w-4/5 bg-gray-300 rounded overflow-hidden">
         <div id="progressBar" className="h-5 w-0 bg-green"></div>
       </div>
